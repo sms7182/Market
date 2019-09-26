87088bad81db231cce19ca46388002971b2e6c05
Shabnam Ranjgar <shabnam.ranjgar@live.com>
2019-09-26 17:50:40 +0330

    Adding Store

---
+++ MarketService/Interfaces/IStoreRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MarketService.Models;
+using MarketService.Models.Contracts;
+
+namespace MarketService.Interfaces
+{
+   public interface IStoreRepository
+    {       
+        StoreInfo Find(Guid id);
+        List<StoreInfo> GetStores();
+    }
+}
